3a96a4e406bfa7318fa90d589c728db9bb44c1fa
mango <23057108+mangosroom@users.noreply.github.com>
2022-04-30 09:36:48 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,8 +4,6 @@ git clone -b "openwrt-21.02" --single-branch https://github.com/immortalwrt/immo
 
 cd ${OP_BUILD_PATH}/immortalwrt
 ./scripts/feeds update -a && ./scripts/feeds install -a
-rm -rf ./tmp && rm -rf .config
-mv ${OP_BUILD_PATH}/ssr.config ${OP_BUILD_PATH}/immortalwrt/.config
 sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 make defconfig
 make -j64 download
